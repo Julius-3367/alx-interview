134f8ac960ae4c2e41ffa6bf6847ba8b76d31359
Julius-3367 <korirjulius001@gmail.com>
2024-05-09 02:07:34 -0700

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
-""" Minimum Operations
-    """
-
+"""
+Minimum Operations
+"""
 
 def minOperations(n: int) -> int:
-    """ Minimum Operations needed to get n H characters """
-    next = 'H'
-    body = 'H'
-    op = 0
-    while (len(body) < n):
-        if n % len(body) == 0:
-            op += 2
-            next = body
-            body += body
-        else:
-            op += 1
-            body += next
-    if len(body) != n:
+    """
+    Minimum Operations needed to get n H characters
+    """
+    if n <= 1:
         return 0
+    
+    op = 0
+    copy = 1
+    paste = 0
+
+    while paste < n:
+        if n % copy == 0:
+            paste = copy
+            op += 1
+        copy += paste
+        op += 1
+
     return op
+
+if __name__ == "__main__":
+    n = 4
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
+    n = 12
+    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
+
